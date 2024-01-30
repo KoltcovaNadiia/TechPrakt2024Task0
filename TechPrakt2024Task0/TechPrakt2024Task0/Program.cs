80971c994c4649b35fc9888de7ad1f73f22a6ac9
Кольцова Надія Олегівна <ipz224_kno@student.ztu.edu.ua>
2024-01-30 05:29:37 +0200

    Додано TechPrakt2024Task0/ та .gitignore

---
+++ TechPrakt2024Task0/TechPrakt2024Task0/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+
+namespace TechPrakt2024Task0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            
+            Console.Write("Введіть перше число: ");
+            if (double.TryParse(Console.ReadLine(), out double number1))
+            {
+                Console.Write("Введіть друге число: ");
+                if (double.TryParse(Console.ReadLine(), out double number2))
+                {
+                    double sum = number1 + number2;
+
+                    Console.WriteLine($"Сума чисел {number1} і {number2} дорівнює: {sum}");
+                }
+                else
+                {
+                    Console.WriteLine("Некоректне друге число.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Некоректне перше число.");
+            }
+        }
+    }
+}
